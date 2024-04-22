371b5b9188130d00d07391791429fc3387e6911d
Alan Firmiano <alanfirmiano18@gmail.com>
2024-04-22 00:04:59 -0300

    fix

--- src/main/kotlin/com/template/apptemplate/repository/TemplateRepository.kt
+++ src/main/kotlin/com/template/apptemplate/repository/TemplateRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TemplateRepository: JpaRepository<TemplateModel, Long> {
-}
+interface TemplateRepository: JpaRepository<TemplateModel, Long>
